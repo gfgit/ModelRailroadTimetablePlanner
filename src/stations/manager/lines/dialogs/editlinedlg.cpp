a4483a0f9c6ad6bfc5ef8735a880c7a69db57db7
Filippo Gentile <gentilefilippo01@gmail.com>
2021-04-08 09:09:00 +0200

    EditLineDlg: split line details and line path, support adding/removing segments and editing line details.

--- src/stations/manager/lines/dialogs/editlinedlg.cpp
+++ src/stations/manager/lines/dialogs/editlinedlg.cpp
@@ -1,42 +1,193 @@
 #include "editlinedlg.h"
 
-#include <QVBoxLayout>
+#include <QBoxLayout>
+#include <QFormLayout>
+
+#include <QLineEdit>
+#include "utils/spinbox/kmspinbox.h"
 #include <QTableView>
+#include <QToolButton>
+
+#include <QTabWidget>
 #include <QDialogButtonBox>
 
+#include <QMessageBox>
+
 #include "stations/manager/lines/model/linesegmentsmodel.h"
+#include "stations/manager/lines/dialogs/choosesegmentdlg.h"
+
+#include <QPointer>
+
 
 EditLineDlg::EditLineDlg(sqlite3pp::database &db, QWidget *parent) :
-    QDialog(parent)
+    QDialog(parent),
+    mDb(db)
 {
-    model = new LineSegmentsModel(db, this);
+    model = new LineSegmentsModel(mDb, this);
 
-    QVBoxLayout *lay = new QVBoxLayout(this);
+    //Details Tab
+    QWidget *detailsTab = new QWidget;
+    QFormLayout *detailsTabLay = new QFormLayout(detailsTab);
+
+    lineNameEdit = new QLineEdit;
+    lineNameEdit->setPlaceholderText(tr("Name..."));
+    detailsTabLay->addRow(tr("Name:"), lineNameEdit);
+
+    lineStartKmSpin = new KmSpinBox;
+    detailsTabLay->addRow(tr("Start at Km:"), lineStartKmSpin);
+
+    //Path Tab
+    QWidget *pathTab = new QWidget;
+    QVBoxLayout *pathTabLay = new QVBoxLayout(pathTab);
+
+    //Buttons
+    QHBoxLayout *toolsLay = new QHBoxLayout;
+
+    QToolButton *addStationBut = new QToolButton;
+    addStationBut->setText(tr("Add station"));
+    toolsLay->addWidget(addStationBut);
+    QToolButton *removeFromPosBut = new QToolButton;
+    removeFromPosBut->setText(tr("Remove After"));
+    toolsLay->addWidget(removeFromPosBut);
+    toolsLay->addStretch();
+
+    pathTabLay->addLayout(toolsLay);
 
     view = new QTableView;
     view->setModel(model);
 
-    lay->addWidget(view);
+    pathTabLay->addWidget(view);
+
+    QVBoxLayout *lay = new QVBoxLayout(this);
+    QTabWidget *tabWidget = new QTabWidget;
+    tabWidget->addTab(detailsTab, tr("Details"));
+    tabWidget->addTab(pathTab, tr("Path"));
+    lay->addWidget(tabWidget);
 
     QDialogButtonBox *box = new QDialogButtonBox(QDialogButtonBox::Ok, Qt::Horizontal);
     lay->addWidget(box);
 
+    connect(model, &LineSegmentsModel::modelError, this, &EditLineDlg::onModelError);
+
     connect(box, &QDialogButtonBox::accepted, this, &EditLineDlg::accept);
     connect(box, &QDialogButtonBox::rejected, this, &EditLineDlg::reject);
 
+    connect(addStationBut, &QToolButton::clicked, this, &EditLineDlg::addStation);
+    connect(removeFromPosBut, &QToolButton::clicked, this, &EditLineDlg::removeAfterCurrentPos);
+
     setWindowTitle(tr("Edit Line"));
     setMinimumSize(500, 300);
+}
+
+void EditLineDlg::done(int res)
+{
+    //FIXME: cannot cancel editings made to path so always accept
+    res = QDialog::Accepted;
+
+    if(res == QDialog::Accepted)
+    {
+        QString lineName = lineNameEdit->text().simplified();
+        int startMeters = lineStartKmSpin->value();
+
+        if(lineName.isEmpty())
+        {
+            onModelError(tr("Line name cannot be empty."));
+            return;
+        }
+
+        if(!model->setLineInfo(lineName, startMeters))
+        {
+            return; //Error
+        }
+    }
+
+    QDialog::done(res);
 }
 
 void EditLineDlg::setLineId(db_id lineId)
 {
     model->setLine(lineId);
 
-    //TODO: Reload UI
+    //Reload UI
     QString lineName;
     int startMeters = 0;
     if(!model->getLineInfo(lineName, startMeters))
     {
         return; //Error
     }
+
+    lineNameEdit->setText(lineName);
+    lineStartKmSpin->setValue(startMeters);
+}
+
+void EditLineDlg::onModelError(const QString &msg)
+{
+    QMessageBox::warning(this, tr("Error"), msg);
+}
+
+void EditLineDlg::addStation()
+{
+    if(model->getSegmentCount() >= MaxSegmentsPerLine)
+    {
+        QMessageBox::warning(this, tr("Error"),
+                             tr("Cannot add another segment to line <b>%1</b>.<br>"
+                                "Each line can have a maximum of <b>%2 segments</b>.")
+                             .arg(lineNameEdit->text())
+                                 .arg(MaxSegmentsPerLine));
+        return;
+    }
+
+    db_id lastStationId = model->getLastStation();
+    db_id lastSegmentId = model->getLastRailwaySegment();
+
+    QPointer<ChooseSegmentDlg> dlg(new ChooseSegmentDlg(mDb, this));
+    dlg->setFilter(lastStationId, ChooseSegmentDlg::DoNotLock, lastSegmentId);
+    int ret = dlg->exec();
+    if(ret != QDialog::Accepted || !dlg)
+        return;
+
+    db_id segmentId = 0;
+    QString segmentName;
+    bool isReversed = false;
+
+    if(dlg->getData(segmentId, segmentName, isReversed))
+    {
+        model->addStation(segmentId, isReversed);
+    }
+
+    delete dlg;
+}
+
+void EditLineDlg::removeAfterCurrentPos()
+{
+    if(!view->selectionModel()->hasSelection())
+    {
+        onModelError(tr("Please select a segment to remove.\n"
+                        "All segments after it are also removed."));
+        return;
+    }
+
+    QModelIndex idx = view->currentIndex();
+    if(!idx.isValid())
+        return;
+
+    int pos = model->getItemIndex(idx.row());
+    const int segmentCount = model->getSegmentCount();
+    if(pos >= segmentCount)
+        pos = segmentCount - 1; //Remove only the last one
+    const QString stationName = model->getStationNameAt(pos);
+
+    const QString msg =
+        tr("Remove last <b>%1</b> segments?"
+           "%2")
+            .arg(segmentCount - pos)
+            .arg(stationName.isEmpty()
+                     ? QString()
+                     : tr("<br>(From station <b>%1</b>)").arg(stationName));
+
+    int ret = QMessageBox::question(this, tr("Are you sure?"), msg);
+    if(ret != QMessageBox::Yes)
+        return;
+
+    model->removeSegmentsAfterPosInclusive(pos);
 }
--- src/stations/manager/lines/dialogs/editlinedlg.h
+++ src/stations/manager/lines/dialogs/editlinedlg.h
@@ -10,6 +10,8 @@ class database;
 }
 
 class QTableView;
+class QLineEdit;
+class KmSpinBox;
 
 class LineSegmentsModel;
 
@@ -19,12 +21,22 @@ class EditLineDlg : public QDialog
 public:
     EditLineDlg(sqlite3pp::database &db, QWidget *parent = nullptr);
 
+    virtual void done(int res) override;
+
     void setLineId(db_id lineId);
 
+private slots:
+    void onModelError(const QString& msg);
+    void addStation();
+    void removeAfterCurrentPos();
+
 private:
+    sqlite3pp::database& mDb;
     LineSegmentsModel *model;
 
     QTableView *view;
+    QLineEdit *lineNameEdit;
+    KmSpinBox *lineStartKmSpin;
 };
 
 #endif // EDITLINEDLG_H
