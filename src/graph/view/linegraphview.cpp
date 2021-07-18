cdfb447800def3428da76c419f3bf44d6f885576
Filippo Gentile <gentilefilippo01@gmail.com>
2021-07-18 23:27:25 +0200

    LineGraphView: drawBackgroundHourLines
    Draw visible hour lines in the background.

--- src/graph/view/linegraphview.cpp
+++ src/graph/view/linegraphview.cpp
@@ -12,6 +12,8 @@
 
 #include <QScrollBar>
 
+#include <QtMath>
+
 LineGraphView::LineGraphView(QWidget *parent) :
     QAbstractScrollArea(parent)
 {
@@ -82,7 +84,12 @@ void LineGraphView::paintEvent(QPaintEvent *e)
     QPainter painter(viewport());
 
     //Scroll contents
-    painter.translate(-horizontalScrollBar()->value(), -verticalScrollBar()->value());
+    const QPoint origin(-horizontalScrollBar()->value(), -verticalScrollBar()->value());
+
+    painter.translate(origin);
+
+    QRect visibleRect(-origin, viewport()->size());
+    drawBackgroundHourLines(&painter, visibleRect);
 
     if(!m_scene || m_scene->getGraphType() == LineGraphType::NoGraph)
         return; //Nothing to draw
@@ -165,6 +172,47 @@ void LineGraphView::ensureVisible(int x, int y, int xmargin, int ymargin)
     } else if (y > vbar->value() + vp->height() - ymargin) {
         vbar->setValue(qMin(y - vp->height() + ymargin, vbar->maximum()));
     }
+}
+
+void LineGraphView::drawBackgroundHourLines(QPainter *painter, const QRectF &rect)
+{
+    const double vertOffset = Session->vertOffset;
+    const double hourOffset = Session->hourOffset;
+    const double hourHorizOffset = AppSettings.getHourLineOffset();
+
+    QPen hourLinePen(AppSettings.getHourLineColor(), AppSettings.getHourLineWidth());
+
+    const qreal x1 = qMax(qreal(hourHorizOffset), rect.left());
+    const qreal x2 = rect.right();
+    const qreal t = qMax(rect.top(), vertOffset);
+    const qreal b = rect.bottom();
+
+
+    if(x1 > x2 || b < vertOffset || t > b)
+        return;
+
+    qreal f = std::remainder(t - vertOffset, hourOffset);
+
+    if(f < 0)
+        f += hourOffset;
+    qreal f1 = qFuzzyIsNull(f) ? vertOffset : qMax(t - f + hourOffset, vertOffset);
+
+
+    const qreal l = std::remainder(b - vertOffset, hourOffset);
+    const qreal l1 = b - l;
+
+    std::size_t n = std::size_t((l1 - f1)/hourOffset) + 1;
+
+    QLineF *arr = new QLineF[n];
+    for(std::size_t i = 0; i < n; i++)
+    {
+        arr[i] = QLineF(x1, f1, x2, f1);
+        f1 += hourOffset;
+    }
+
+    painter->setPen(hourLinePen);
+    painter->drawLines(arr, int(n));
+    delete [] arr;
 }
 
 void LineGraphView::paintStations(QPainter *painter)
