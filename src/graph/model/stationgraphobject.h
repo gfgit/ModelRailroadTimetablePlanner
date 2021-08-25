04159bbca27973280f4ddb2bd668f90f3f4f670a
Filippo Gentile <gentilefilippo01@gmail.com>
2021-08-25 17:04:46 +0200

    Add struct JobLineGraph to draw moving job graph
    * Rename job stop graph to JobStopGraph
    
    * Document code

--- src/graph/model/stationgraphobject.h
+++ src/graph/model/stationgraphobject.h
@@ -8,6 +8,11 @@
 #include "stations/station_utils.h"
 #include <QRgb>
 
+/*!
+ * Graph of a railway station
+ *
+ * Contains informations to draw station name, platforms and jobs
+ */
 class StationGraphObject
 {
 public:
@@ -17,22 +22,52 @@ public:
     QString stationName;
     utils::StationType stationType;
 
+    /*!
+     * Graph of the job while is stopping
+     *
+     * Contains informations to draw job line on top of the PlatformGraph
+     */
     typedef struct
     {
         db_id jobId;
-        db_id stopId;
         JobCategory category;
+
+        db_id stopId;
         double arrivalY;
         double departureY;
-    } JobGraph;
+    } JobStopGraph;
 
+    /*!
+     * Graph of the job while is moving
+     *
+     * Contains informations to draw job line between two adjacent stations
+     */
+    typedef struct
+    {
+        db_id jobId;
+        JobCategory category;
+
+        db_id fromStopId;
+        db_id fromPlatfId;
+        double fromDepartureY;
+
+        db_id toStopId;
+        db_id toPlatfId;
+        double toArrivalY;
+    } JobLineGraph;
+
+    /*!
+     * Graph of a station track (platform)
+     *
+     * Contains informations to draw platform line and header name
+     */
     typedef struct
     {
         db_id platformId;
         QString platformName;
         QRgb color;
         QFlags<utils::StationTrackType> platformType;
-        QVector<JobGraph> jobStops;
+        QVector<JobStopGraph> jobStops;
     } PlatformGraph;
 
     QVector<PlatformGraph> platforms;
