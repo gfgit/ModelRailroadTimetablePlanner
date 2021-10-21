fb527db6afd634ad591b47db85a7f47458f123b9
Filippo Gentile <gentilefilippo01@gmail.com>
2021-10-21 23:43:51 +0200

    GraphManager: old deprecated class, remove some code

--- src/graph/graphmanager.h
+++ src/graph/graphmanager.h
@@ -5,8 +5,6 @@
 
 #include "utils/types.h"
 
-class LineStorage;
-
 typedef struct JobSelection
 {
     db_id jobId;
@@ -18,39 +16,17 @@ class GraphManager : public QObject
     Q_OBJECT
 public:
     explicit GraphManager(QObject *parent = nullptr);
-    ~GraphManager();
 
     db_id getCurLineId() const;
 
     JobSelection getSelectedJob();
     void clearSelection();
 
-    db_id getFirstLineId();
-
 signals:
-    void currentLineChanged(db_id lineId);
-
     void jobSelected(db_id jobId);
 
-public slots:
-    void onSelectionChanged();
-    void onSelectionCleared();
-
-private slots:
-    void onLineNameChanged(db_id lineId);
-    void onLineModified(db_id lineId);
-    void onLineRemoved(db_id lineId);
-
-    void updateGraphOptions();
-
-public:
-    LineStorage *lineStorage;
-
 private:
-
     db_id curLineId;
-
-    db_id curJobId;
 };
 
 #endif // GRAPHMANAGER_H
