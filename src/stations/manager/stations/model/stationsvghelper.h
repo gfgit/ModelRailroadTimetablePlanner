b9b168bb1078a7cda6373d77dd367094ebc59abd
Filippo Gentile <gentilefilippo01@gmail.com>
2021-10-21 10:32:58 +0200

    StationSVGHelper: load StationPlan data from database

--- src/stations/manager/stations/model/stationsvghelper.h
+++ src/stations/manager/stations/model/stationsvghelper.h
@@ -9,6 +9,10 @@ namespace sqlite3pp {
 class database;
 }
 
+namespace ssplib {
+class StationPlan;
+}
+
 //FIXME: compress SVG on saving and uncompress on loading?
 class StationSVGHelper
 {
@@ -19,6 +23,8 @@ public:
 
     static bool saveImage(sqlite3pp::database &db, db_id stationId, QIODevice *dest);
     static QIODevice* loadImage(sqlite3pp::database &db, db_id stationId);
+
+    static bool loadStationFromDB(sqlite3pp::database &db, db_id stationId, ssplib::StationPlan *plan);
 };
 
 #endif // STATIONSVGHELPER_H
