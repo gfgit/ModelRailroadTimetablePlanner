e868e4e20cd455b455d4b9003e94c13e4060bea0
Filippo Gentile <gentilefilippo01@gmail.com>
2021-12-27 12:31:03 +0100

    OpenFileInFolderDlg: new dialog to open containing folder
    Do not use QMessageBox to avoid closing when click buttons

--- src/utils/openfileinfolder.cpp
+++ src/utils/openfileinfolder.cpp
@@ -1,24 +1,22 @@
 #include "openfileinfolder.h"
 
-#include <QCoreApplication>
 #include <QFileInfo>
+#include <QDir>
 #include <QUrl>
 
 #include <QProcess>
 #include <QDesktopServices>
 
-#include "utils/owningqpointer.h"
-#include <QMessageBox>
+#include <QVBoxLayout>
 #include <QPushButton>
+#include <QLabel>
+#include <QDialogButtonBox>
+
+#include "utils/owningqpointer.h"
 
 namespace utils {
 
-class OpenFileInFolder
-{
-    Q_DECLARE_TR_FUNCTIONS(OpenFileInFolder)
-};
-
-bool openFileInFolder(const QFileInfo& info, bool folder)
+static bool openDefAppOrShowFolder(const QFileInfo& info, bool folder)
 {
     if(info.isDir())
         folder = true;
@@ -26,44 +24,76 @@ bool openFileInFolder(const QFileInfo& info, bool folder)
     QString path = folder ? info.absolutePath() : info.absoluteFilePath();
 
 #if defined(Q_OS_WIN) && !defined (Q_OS_WINRT)
-    if(!folder)
+    if(folder && !info.isDir())
     {
-        QString args = QStringLiteral("/select,%1").arg(path);
+        //NOTE: on Windows desktop we can also select a file inside a folder
+        //So prefer it instead of default QDesktopServices
+        QString args = QStringLiteral("/select,%1").arg(QDir::toNativeSeparators(info.absoluteFilePath()));
         QProcess explorer;
         explorer.setProgram(QLatin1String("explorer.exe"));
         explorer.setNativeArguments(args); //This must not be quoted
-        return explorer.startDetached();
+        if(explorer.startDetached())
+            return true;
     }
 #endif
 
     return QDesktopServices::openUrl(QUrl::fromLocalFile(path));
 }
 
-} //namespace utils
-
-void utils::openFileInFolder(const QString &title, const QString &filePath, QWidget *parent)
+OpenFileInFolderDlg::OpenFileInFolderDlg(QWidget *parent) :
+    QDialog(parent)
 {
-    QFileInfo info(filePath);
+    QVBoxLayout *lay = new QVBoxLayout(this);
 
-    OwningQPointer<QMessageBox> msgBox = new QMessageBox(parent);
-    msgBox->setIcon(QMessageBox::Information);
-    msgBox->setWindowTitle(title);
-    msgBox->setText(OpenFileInFolder::tr("Do you want to open file?<br><b>%1</b>").arg(info.canonicalFilePath()));
+    mLabel = new QLabel;
+    lay->addWidget(mLabel);
 
-    QPushButton *openFileBut = nullptr;
-    if(info.isFile())
-        openFileBut = msgBox->addButton(OpenFileInFolder::tr("Open In App"), QMessageBox::AcceptRole);
-    QPushButton *folderBut = msgBox->addButton(OpenFileInFolder::tr("Show Folder"), QMessageBox::AcceptRole);
-    msgBox->addButton(QMessageBox::Ok);
-    if(msgBox->exec() != QMessageBox::Accepted || !msgBox)
-        return;
+    QDialogButtonBox *box = new QDialogButtonBox(Qt::Horizontal);
+    lay->addWidget(box);
 
-    if(openFileBut && msgBox->clickedButton() == openFileBut)
-    {
-        openFileInFolder(info, false);
-    }
-    else if(msgBox->clickedButton() == folderBut)
-    {
-        openFileInFolder(info, true);
-    }
+    openFileBut = box->addButton(tr("Open In App"), QDialogButtonBox::AcceptRole);
+    openFolderBut = box->addButton(tr("Show Folder"), QDialogButtonBox::AcceptRole);
+    QPushButton *okBut = box->addButton(QDialogButtonBox::Ok);
+
+    //Open Folder by default
+    openFolderBut->setDefault(true);
+    openFolderBut->setFocus();
+
+    //Only close dialog with Ok button, ignore others
+    connect(okBut, &QPushButton::clicked, this, &QDialog::accept);
+    connect(openFileBut, &QPushButton::clicked, this, &OpenFileInFolderDlg::onOpenFile);
+    connect(openFolderBut, &QPushButton::clicked, this, &OpenFileInFolderDlg::onOpenFolder);
 }
+
+void OpenFileInFolderDlg::setFilePath(const QString &newFilePath)
+{
+    info.setFile(newFilePath);
+    openFileBut->setVisible(info.isFile());
+}
+
+void OpenFileInFolderDlg::setLabelText(const QString &text)
+{
+    mLabel->setText(text);
+}
+
+void OpenFileInFolderDlg::askUser(const QString &title, const QString &filePath, QWidget *parent)
+{
+    OwningQPointer<OpenFileInFolderDlg> dlg = new OpenFileInFolderDlg(parent);
+    dlg->setWindowTitle(title);
+    dlg->setFilePath(filePath);
+    dlg->setLabelText(OpenFileInFolderDlg::tr("Do you want to open file?<br><b>%1</b>")
+                          .arg(dlg->getInfo().canonicalFilePath()));
+    dlg->exec();
+}
+
+void OpenFileInFolderDlg::onOpenFile()
+{
+    openDefAppOrShowFolder(info, false);
+}
+
+void OpenFileInFolderDlg::onOpenFolder()
+{
+    openDefAppOrShowFolder(info, true);
+}
+
+} //namespace utils
--- src/utils/openfileinfolder.h
+++ src/utils/openfileinfolder.h
@@ -1,13 +1,59 @@
 #ifndef OPENFILEINFOLDER_H
 #define OPENFILEINFOLDER_H
 
-#include <QString>
+#include <QDialog>
 
-class QWidget;
+#include <QFileInfo>
+
+class QLabel;
+class QPushButton;
 
 namespace utils {
 
-void openFileInFolder(const QString& title, const QString& filePath, QWidget *parent);
+/*!
+ * \brief The OpenFileInFolderDlg class
+ *
+ * A small dialog to open file of folder in default app or show its
+ * containing folder in system file manager
+ *
+ * This does not use QMessageBox because it would close
+ * when user clicks any button.
+ * This dialogs instead closes only with Ok button
+ *
+ * Windows: special case, it can also select a file inside explorer.exe
+ */
+class OpenFileInFolderDlg : public QDialog
+{
+    Q_OBJECT
+public:
+    OpenFileInFolderDlg(QWidget *parent = nullptr);
+
+    void setFilePath(const QString &newFilePath);
+    void setLabelText(const QString &text);
+
+    inline QFileInfo getInfo() const { return info; }
+
+    /*!
+     * \brief askUser
+     * \param title dialog title
+     * \param filePath file or folder to open
+     * \param parent parent window
+     *
+     * This convinience function opens standard dialog with default text
+     */
+    static void askUser(const QString& title, const QString& filePath, QWidget *parent);
+
+private slots:
+    void onOpenFile();
+    void onOpenFolder();
+
+private:
+    QLabel *mLabel;
+    QPushButton *openFileBut;
+    QPushButton *openFolderBut;
+
+    QFileInfo info;
+};
 
 }
 
