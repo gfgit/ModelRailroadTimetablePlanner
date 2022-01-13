e99ede2470326155829d0866f659f6979aa5554a
Filippo Gentile <gentilefilippo01@gmail.com>
2022-01-13 11:39:55 +0100

    RSListOnDemandModel: port to IPagedItemModelImpl common code
    - Removed now unused RSListOnDemandResultEvent class
    - Adapt TrainAssetModel and StopCouplingModel

--- src/jobs/jobeditor/model/rslistondemandmodel.h
+++ src/jobs/jobeditor/model/rslistondemandmodel.h
@@ -1,59 +1,43 @@
 #ifndef RSLISTONDEMANDMODEL_H
 #define RSLISTONDEMANDMODEL_H
 
-#include "utils/delegates/sql/pageditemmodel.h"
+#include "utils/delegates/sql/pageditemmodelhelper.h"
 
 #include "utils/types.h"
 
 #include <QVector>
 
+struct RSListOnDemandModelItem
+{
+    db_id rsId;
+    QString name;
+    RsType type;
+};
 
-class RSListOnDemandModel : public IPagedItemModel
+class RSListOnDemandModel : public IPagedItemModelImpl<RSListOnDemandModel, RSListOnDemandModelItem>
 {
     Q_OBJECT
 
 public:
+    enum { BatchSize = 50 };
 
-    enum { BatchSize = 25 };
-
-    typedef enum {
+    enum Columns {
         Name = 0,
         NCols
-    } Columns;
+    };
 
-    typedef struct RSItem_
-    {
-        db_id rsId;
-        QString name;
-        RsType type;
-    } RSItem;
+    typedef RSListOnDemandModelItem RSItem;
+    typedef IPagedItemModelImpl<RSListOnDemandModel, RSListOnDemandModelItem> BaseClass;
 
     RSListOnDemandModel(sqlite3pp::database &db, QObject *parent = nullptr);
 
-    bool event(QEvent *e) override;
-
     // QAbstractTableModel
-
-    // Basic functionality:
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
 
     QVariant data(const QModelIndex &idx, int role = Qt::DisplayRole) const override;
 
-    // IPagedItemModel
-
-    // Cached rows management
-    virtual void clearCache() override;
-
 private:
+    friend BaseClass;
     virtual void internalFetch(int first, int sortColumn, int valRow, const QVariant &val) = 0;
-    void fetchRow(int row);
-    void handleResult(const QVector<RSItem> &items, int firstRow);
-
-private:
-    QVector<RSItem> cache;
-    int cacheFirstRow;
-    int firstPendingRow;
 };
 
 #endif // RSLISTONDEMANDMODEL_H
