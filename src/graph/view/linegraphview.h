9562a667eb0fc6f74a36cb31c36b0a0d548d9a6f
Filippo Gentile <gentilefilippo01@gmail.com>
2022-01-20 20:18:06 +0100

    LineGraphView: subclass BasicGraphView

--- src/graph/view/linegraphview.h
+++ src/graph/view/linegraphview.h
@@ -1,44 +1,24 @@
 #ifndef LINEGRAPHVIEW_H
 #define LINEGRAPHVIEW_H
 
-#include <QAbstractScrollArea>
-
-class LineGraphScene;
-
-class StationLabelsHeader;
-class HourPanel;
+#include "utils/scene/basicgraphview.h"
 
 /*!
- * \brief Widget to display a LineGraphScene
+ * \brief BasicGraphView subclass to display a LineGraphScene
  *
- * A scrollable widget which renders LineGraphScene contents
- * Moving the mouse cursor on the contents, tooltips will show
+ * A custom view to render LineGraphScene contents.
+ * Moving the mouse cursor on the contents, tooltips will be shown.
+ * Double clicking on a Job selects it.
  *
+ * \sa LineGraphScene
  * \sa LineGraphWidget
  * \sa BackgroundHelper
  */
-class LineGraphView : public QAbstractScrollArea
+class LineGraphView : public BasicGraphView
 {
     Q_OBJECT
 public:
     explicit LineGraphView(QWidget *parent = nullptr);
-
-    LineGraphScene *scene() const;
-    void setScene(LineGraphScene *newScene);
-
-    /*!
-     * \brief ensure point is visible
-     *
-     * Scrolls the contents of the scroll area so that the point (\a x, \a y) is visible
-     * inside the region of the viewport with margins specified in pixels by \a xmargin and
-     * \a ymargin. If the specified point cannot be reached, the contents are scrolled to
-     * the nearest valid position. The default value for both margins is 50 pixels.
-     *
-     * Vertical and horizontal headers are excluded from the visible zone
-    */
-    void ensureVisible(double x, double y, double xmargin, double ymargin);
-
-    inline int getZoomLevel() const { return mZoom; }
 
 signals:
     /*!
@@ -48,34 +28,7 @@ signals:
      */
     void syncToolbarToScene();
 
-    void zoomLevelChanged(int zoom);
-
-public slots:
-    /*!
-     * \brief Triggers graph redrawing
-     *
-     * \sa updateScrollBars()
-     */
-    void redrawGraph();
-
-    /*!
-     * \brief ensure a rect is visible in the viewport
-     *
-     * \sa ensureVisible()
-     */
-    void ensureRectVisible(const QRectF& r);
-
-    void setZoomLevel(int zoom);
-
 protected:
-    /*!
-     * \brief React to layout and style change
-     *
-     * On layout and style change update scroll bars
-     * \sa updateScrollBars()
-     */
-    bool event(QEvent *e) override;
-
     /*!
      * \brief Show Tooltips
      *
@@ -83,9 +36,6 @@ protected:
      * \sa LineGraphScene::getJobAt()
      */
     bool viewportEvent(QEvent *e) override;
-
-    void paintEvent(QPaintEvent *e) override;
-    void resizeEvent(QResizeEvent *) override;
 
     /*!
      * \brief Sync toolbar on click
@@ -101,35 +51,6 @@ protected:
      * \sa LineGraphScene::getJobAt()
      */
     void mouseDoubleClickEvent(QMouseEvent *e) override;
-
-    /*!
-     * \brief Activate view
-     *
-     * This view (and its scene) is now active
-     * It will receive requests to show items
-     *
-     * \sa LineGraphScene::activateScene()
-     * \sa LineGraphManager::setActiveScene()
-     */
-    void focusInEvent(QFocusEvent *e) override;
-
-private slots:
-    void onSceneDestroyed();
-    void resizeHeaders();
-
-private:
-    /*!
-     * \brief Update scrollbar size
-     */
-    void updateScrollBars();
-
-private:
-    StationLabelsHeader *stationHeader;
-    HourPanel *hourPanel;
-
-    LineGraphScene *m_scene;
-
-    int mZoom;
 };
 
 #endif // LINEGRAPHVIEW_H
