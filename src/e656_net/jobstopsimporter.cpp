fc5d4434dab63375b40c4b2de7ae860c61b6b210
Filippo Gentile <gentilefilippo01@gmail.com>
2022-11-01 11:15:50 +0100

    JobStopsImporter: fix job creation

--- src/e656_net/jobstopsimporter.cpp
+++ src/e656_net/jobstopsimporter.cpp
@@ -37,6 +37,12 @@ bool JobStopsImporter::createJob(db_id jobId, JobCategory category, const QVecto
     int i = 0;
     for(const Stop& row : stops)
     {
+        //Get previous stop from model
+        StopItem prevStop;
+        if(i > 0)
+            prevStop = model.getItemAt(i - 1);
+
+        //Parse station
         q_getStId.bind(1, row.stationName.toUpper());
         if(q_getStId.step() != SQLITE_ROW)
         {
@@ -50,17 +56,47 @@ bool JobStopsImporter::createJob(db_id jobId, JobCategory category, const QVecto
         db_id stationId = q_getStId.getRows().get<db_id>(0);
         q_getStId.reset();
 
+        //Find segment between previous stop and current
+        db_id prevSeg = 0;
+        if(prevStop.stationId)
+        {
+            q_getSeg.bind(1, prevStop.stationId);
+            q_getSeg.bind(2, stationId);
+            int ret = q_getSeg.step();
+            prevSeg = q_getSeg.getRows().get<db_id>(0);
+            if(ret != SQLITE_ROW || !prevSeg)
+            {
+                qDebug() << "Error Job:" << jobId << "Stations:" << prevStop.stationId << "and" << stationId << "not connected";
+                qDebug() << mDb.error_msg();
+                break;
+            }
+            q_getSeg.reset();
+        }
+
+        //Create new (current) stop
         model.addStop();
 
-        StopItem curStop = model.getItemAt(i);
-        StopItem prevStop;
-        if(i > 0)
-            prevStop = model.getItemAt(i - 1);
+        //Set next segment of previous stop (now is not Last anymore)
+        db_id outSegGateId = 0;
+        if(prevSeg)
+        {
+            if(model.trySelectNextSegment(prevStop, prevSeg, 0, stationId, outSegGateId))
+            {
+                StopItem::Segment beforePrevSeg;
+                if(i > 1)
+                    beforePrevSeg = model.getItemAt(i - 2).nextSegment;
 
+                //Store changes to previous stop before editing current one
+                model.setStopInfo(model.index(i - 1), prevStop, beforePrevSeg);
+            }
+        }
+
+        //Get stop after saving previous
+        StopItem curStop = model.getItemAt(i);
+        curStop.stationId = stationId;
         curStop.arrival = row.arrival;
         curStop.departure = row.departure;
 
-        curStop.stationId = stationId;
         model.trySelectTrackForStop(curStop);
 
         db_id newTrackId = 0;
@@ -82,34 +118,19 @@ bool JobStopsImporter::createJob(db_id jobId, JobCategory category, const QVecto
             }
         }
 
-        db_id prevSeg = 0;
-        if(prevStop.stationId)
-        {
-            q_getSeg.bind(1, prevStop.stationId);
-            q_getSeg.bind(2, curStop.stationId);
-            int ret = q_getSeg.step();
-            if(ret != SQLITE_ROW)
-            {
-                qDebug() << "Error Job:" << jobId << "Stations:" << prevStop.stationId << "and" << curStop.stationId << "not connected";
-                qDebug() << mDb.error_msg();
-            }
-            prevSeg = q_getSeg.getRows().get<db_id>(0);
-            q_getSeg.reset();
-        }
-
-        db_id outSegGateId = 0;
-        if(prevSeg)
-        {
-            if(!model.trySelectNextSegment(prevStop, prevSeg, 0, curStop.stationId, outSegGateId))
-            {
-
-            }
-        }
-
-
         model.setStopInfo(model.index(i), curStop, prevStop.nextSegment);
 
         i++;
+    }
+
+    model.commitChanges();
+
+    if(model.rowCount() < 3)
+    {
+        //A Job must have at least 2 stop (origin, destination)
+        //Plus 1 row "Add Here" added by the model
+        JobsHelper::removeJob(mDb, model.getJobId());
+        return false;
     }
 
     return true;
