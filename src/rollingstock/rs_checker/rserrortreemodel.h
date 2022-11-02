576a79e065b317b55138c9d37d6f1b47dc677dbd
Filippo Gentile <gentilefilippo01@gmail.com>
2022-11-02 10:14:48 +0100

    RsErrorTreeModel: port to SingleDepthTreeModelHelper
    Use ENABLE_BACKGROUND_MANAGER guard macro

--- src/rollingstock/rs_checker/rserrortreemodel.h
+++ src/rollingstock/rs_checker/rserrortreemodel.h
@@ -1,20 +1,19 @@
 #ifndef RSERRORTREEMODEL_H
 #define RSERRORTREEMODEL_H
 
-#ifdef ENABLE_RS_CHECKER
+#ifdef ENABLE_BACKGROUND_MANAGER
 
-#include <QAbstractItemModel>
+#include "rs_error_data.h"
+#include "utils/singledepthtreemodelhelper.h"
 
-#include "error_data.h"
+class RsErrorTreeModel;
+typedef SingleDepthTreeModelHelper<RsErrorTreeModel, RsErrors::RSErrorMap, RsErrors::RSErrorData> RsErrorTreeModelBase;
 
 //TODO: make on-demand
-class RsErrorTreeModel : public QAbstractItemModel
+class RsErrorTreeModel : public RsErrorTreeModelBase
 {
     Q_OBJECT
-
 public:
-    typedef QMap<db_id, RsErrors::RSErrorList> Data;
-
     enum Columns
     {
         JobName = 0,
@@ -29,17 +28,6 @@ public:
     // Header:
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
 
-    // Basic functionality:
-    QModelIndex index(int row, int column,
-                      const QModelIndex &parent = QModelIndex()) const override;
-    QModelIndex parent(const QModelIndex &idx) const override;
-
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
-    bool hasChildren(const QModelIndex &parent = QModelIndex()) const override;
-
-    QModelIndex sibling(int row, int column, const QModelIndex &idx) const override;
-
     QVariant data(const QModelIndex &idx, int role = Qt::DisplayRole) const override;
 
     void setErrors(const QMap<db_id, RsErrors::RSErrorList> &data);
@@ -48,15 +36,10 @@ public:
 
     void clear();
 
-    RsErrors::ErrorData *getItem(const QModelIndex &idx) const;
-
 private slots:
     void onRSInfoChanged(db_id rsId);
-
-private:
-    Data m_data;
 };
 
-#endif // ENABLE_RS_CHECKER
+#endif // ENABLE_BACKGROUND_MANAGER
 
 #endif // RSERRORTREEMODEL_H
